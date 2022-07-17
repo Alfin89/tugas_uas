00726ea677bdd86caae07da95107ce5b133d5557
Alfin89 <104522708+Alfin89@users.noreply.github.com>
2022-07-17 22:51:26 +0700

    Add files via upload

---
+++ alfin/mysaya/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def index (request):
+    context = {
+        'judul':'home'
+}
+    return render(request,'index.html',context)
+
+
